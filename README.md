6d1f23dc2fd6b094de940417498e5d12f3f92464
veretennikovalexey <raidex@yandex.ru>
2025-08-11 08:29:29 +0300

    alex

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 https://github.com/veretennikovalexey
 
 gh repo clone veretennikovalexey/doc
+
+pip install mkdocs
+
+mkdocs new .
+
+## mkdocs.yml
+
+theme:
+  name: readthedocs
+
+mkdocs serve
--- pp.cmd
+++ pp.cmd
@@ -3,6 +3,10 @@ git pull
 git config --global user.name veretennikovalexey
 git config --global user.email raidex@yandex.ru
 
+mkdocs build
+
+mkdocs gh-deploy
+
 git add .
 git commit -m alex
 git push origin main
